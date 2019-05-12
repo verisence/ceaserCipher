bb99553a8387224775065788418989fe4dfd3a81
Phil Kipkemboi <Kipkemboiphil@gmail.com>
2019-05-13 00:15:49 +0300

    Make the previous test pass

--- src/main/java/Encrypter.java
+++ src/main/java/Encrypter.java
@@ -1,6 +1,16 @@
 public class Encrypter {
     public static String encrypt(String testWord, int key){
         String cipher = "";
+
+        if(key<1 || key>26){
+            System.out.println("The key should be between 1 and 26");
+        }else{
+            for( int i=0; i<testWord.length(); i++) {
+                char position = testWord.charAt(i);
+                cipher += (char)(position+key);
+            }
+        }
+
         return cipher;
     }
 }
